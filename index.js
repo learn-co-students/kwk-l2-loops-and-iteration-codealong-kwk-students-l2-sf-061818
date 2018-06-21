c51a66cb099f8b52fb9a74e1a75a8340ca7c0535
Areeta Wong <areetaw@gmail.com>
2018-06-21 17:47:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function printBadges(employeeNames) {
+  for (var count = 0; count < employeeNames.length; count++) {
+    console.log("Welcome " + employeeNames[count] + "! " + "You are employee #" + (count+1) + ".");
+  }
+  return employeeNames;
+}
+
+function tailsNeverFails() {
+  var tails = 0;
+  while (Math.random(0) >= 0.5) {
+    tails++;
+  }
+  return "You got " + tails + " tails in a row!";
+}
